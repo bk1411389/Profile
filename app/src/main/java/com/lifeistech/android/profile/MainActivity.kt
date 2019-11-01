d882df4819606dd01f29ad26be7fbaad3234c5b0
Watarai <is0288xs@ed.ritsumei.ac.jp>
2019-11-01 20:45:54 +0900

    自己紹介アプリ完成

---
+++ app/src/main/java/com/lifeistech/android/profile/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.lifeistech.android.profile
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myNameButton.setOnClickListener{
+            myNameButton.setBackgroundColor(Color.parseColor("#4FC7E5"))
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myText.text = "へーる"
+        }
+
+        myLikeSportsButton.setOnClickListener{
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#4FC7E5"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myText.text = "バスケ"
+        }
+
+        myLikeFoodsButton.setOnClickListener{
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#4FC7E5"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#929292"))
+            myText.text = "すし"
+        }
+
+        myLikeHobbyButton.setOnClickListener{
+            myLikeSportsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myNameButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeFoodsButton.setBackgroundColor(Color.parseColor("#929292"))
+            myLikeHobbyButton.setBackgroundColor(Color.parseColor("#4FC7E5"))
+            myText.text = "音楽"
+        }
+
+//        myImageView.visibility = View.INVISIBLE
+//        myText.visibility = View.INVISIBLE
+//
+//        myNameButton.setOnClickListener{
+//            myImageView.visibility = View.VISIBLE
+//            myText.visibility = View.VISIBLE
+//        }
+
+    }
+}
